76e6c75337d07c6ffb05c0b310330cadb4d8ad47
slklab <slawek.klab@gmail.com>
2016-12-07 09:18:26 +0100

    initial commit

---
+++ src/main/java/pl/klab/App.java
@@ -0,0 +1,13 @@
+package pl.klab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
